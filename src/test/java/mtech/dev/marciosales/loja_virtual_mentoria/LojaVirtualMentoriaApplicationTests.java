841b0553c8bfcfb2cac2ca5903fa24b2d776bb2f
marciosalesdev <marcioserrasales2014@gmail.com>
2025-06-29 23:21:33 -0300

    commit inicial projeto loja

---
+++ src/test/java/mtech/dev/marciosales/loja_virtual_mentoria/LojaVirtualMentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package mtech.dev.marciosales.loja_virtual_mentoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualMentoriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
