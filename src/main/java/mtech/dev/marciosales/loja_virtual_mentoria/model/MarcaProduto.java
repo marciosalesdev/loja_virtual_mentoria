9f5334dffa611a4ab48074597d34a4b6dc6e89ac
marciosalesdev <marcioserrasales2014@gmail.com>
2025-06-30 01:32:29 -0300

    commit inicial projeto loja

---
+++ src/main/java/mtech/dev/marciosales/loja_virtual_mentoria/model/MarcaProduto.java
@@ -0,0 +1,42 @@
+package mtech.dev.marciosales.loja_virtual_mentoria.model;
+
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto",sequenceName = "seq_marca_produto",allocationSize = 1, initialValue= 1)
+public class MarcaProduto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_marca_produto"  )
+    private Long id;
+    private String nomeDesc;
+
+    public MarcaProduto() {
+    }
+
+    public MarcaProduto(Long id, String nomeDesc) {
+        this.id = id;
+        this.nomeDesc = nomeDesc;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeDesc() {
+        return nomeDesc;
+    }
+
+    public void setNomeDesc(String nomeDesc) {
+        this.nomeDesc = nomeDesc;
+    }
+}
